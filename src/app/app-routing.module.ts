d982cc556949a4586222eab0fbc0b3324f43d784
Nagendra mohan <62485736+Nagendra07-moh@users.noreply.github.com>
2023-03-13 15:29:11 +0530

    FRONT-END DONE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component:LoginComponent,
+  },
+  {
+    path: "sign-up", component:SignupComponent,
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
