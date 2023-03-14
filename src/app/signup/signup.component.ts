7e5dc410585eb0bd37e7040e2a238ced107965e1
Nagendra mohan <62485736+Nagendra07-moh@users.noreply.github.com>
2023-03-14 12:29:39 +0530

    crud complete

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { SerService } from '../ser.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,11 +9,33 @@ import { Component } from '@angular/core';
 })
 export class SignupComponent {
 
+  constructor(private http:HttpClient, private ser:SerService){
+
+  }
+  obj:any = {};
+
+  sendData() {
+    
+    this.obj.f_name = this.F_name;
+    this.obj.l_name = this.L_name;
+    this.obj.email = this.email;
+    this.obj.pass = this.Pass;
+
+    // console.log(this.obj);
+    this.ser.postMethod(this.obj).subscribe((res)=>{
+      console.log(res);
+    },(err)=>{})
+    
+
+  }
+
+
 
   F_name  = '';
   L_name = '';
   email = '';
   Pass = '';
+
 
 
 }
