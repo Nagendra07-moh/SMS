ecdd16c0b6df28defb54abbe3ec49e744622a9b0
Nagendra mohan <62485736+Nagendra07-moh@users.noreply.github.com>
2023-03-16 15:16:17 +0530

    Searching functionalty done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,17 +14,31 @@ export class HomeComponent {
     ngOnInit(): void {
       this.myser.show().subscribe((data:any)=>{
         this.allContact = data
-        console.log(data)
+        this.TempList = data;
+        // console.log(this.TempList);
+        // console.log(data)
       },(err)=>{
         console.log(err)
       })
     }
+    imp = ''
+    TempList:any[] = []
+    searchData(){
+      if(!this.imp){
+        this.TempList = this.allContact
+      }
+      this.TempList = []
+      console.log(this.imp);
+      let size = this.imp.length;
+      for(let i = 0;i<this.allContact.length;i++){
+        const s =  this.allContact[i].F_name.substring(0,size);
+        if(s.toUpperCase() === this.imp.toUpperCase()){
+          this.TempList.push(this.allContact[i])
+        }
+      }
+    }
 
   
-    imp = ''
-
-  searchData(){
-  }
 
   F_name =  '';
   L_name = '';
